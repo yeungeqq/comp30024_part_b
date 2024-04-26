e4a86049495af85639ce18f7d94427aec58f0ef5
JoeyHammoth <jamesputradi@gmail.com>
2024-04-26 19:19:54 +1000

    Refined Random Testing Agent

--- part_b/testing_agents/agent2/program.py
+++ part_b/testing_agents/agent2/program.py
@@ -1,8 +1,9 @@
-# COMP30024 Artificial Intelligence, Semester 1 2024
-# Project Part B: Game Playing Agent
+# Random Agent
 
+import random
 from referee.game import PlayerColor, Action, PlaceAction, Coord
 
+# Agent2 places actions by randomly selecting 4 adjecent blocks on the board that hasn't been placed yet
 
 class Agent:
     """
@@ -33,26 +34,48 @@ class Agent:
         to take an action. It must always return an action object. 
         """
 
-        # Below we have hardcoded two actions to be played depending on whether
-        # the agent is playing as BLUE or RED. Obviously this won't work beyond
-        # the initial moves of the game, so you should use some game playing
-        # technique(s) to determine the best action to take.
+        # Randomly select 4 adjacent blocks on the board that hasn't been placed yet
+        # Check whether the randomly selected blocks have been placed in the game board
+        place_action_coords = []
+        while len(place_action_coords) < 4:
+            xcoord = random.randint(0, 10)
+            ycoord = random.randint(0, 10)
+            for place_action_piece in self.place_action_list:
+                for coord in place_action_piece.coords:
+                    if Coord(xcoord, ycoord) == coord:
+                        continue
+            if len(place_action_coords) == 0:
+                if Coord(xcoord, ycoord) not in place_action_coords:
+                    place_action_coords.append(Coord(xcoord, ycoord))
+            else:
+                if Coord(xcoord, ycoord) not in place_action_coords:
+                    if xcoord + 1 == place_action_coords[0].r and ycoord == place_action_coords[0].c:
+                        place_action_coords.append(Coord(xcoord, ycoord))
+                    elif xcoord - 1 == place_action_coords[0].r and ycoord == place_action_coords[0].c:
+                        place_action_coords.append(Coord(xcoord, ycoord))
+                    elif xcoord == place_action_coords[0].r and ycoord + 1 == place_action_coords[0].c:
+                        place_action_coords.append(Coord(xcoord, ycoord))
+                    elif xcoord == place_action_coords[0].r and ycoord - 1 == place_action_coords[0].c:
+                        place_action_coords.append(Coord(xcoord, ycoord))
+                    else:
+                        continue
+
         match self._color:
             case PlayerColor.RED:
                 print("Testing: RED is playing a PLACE action")
                 return PlaceAction(
-                    Coord(3, 3), 
-                    Coord(3, 4), 
-                    Coord(4, 3), 
-                    Coord(4, 4)
+                    place_action_coords[0], 
+                    place_action_coords[1], 
+                    place_action_coords[2], 
+                    place_action_coords[3]
                 )
             case PlayerColor.BLUE:
                 print("Testing: BLUE is playing a PLACE action")
                 return PlaceAction(
-                    Coord(2, 3), 
-                    Coord(2, 4), 
-                    Coord(2, 5), 
-                    Coord(2, 6)
+                    place_action_coords[0], 
+                    place_action_coords[1], 
+                    place_action_coords[2], 
+                    place_action_coords[3]
                 )
 
     def update(self, color: PlayerColor, action: Action, **referee: dict):
@@ -81,92 +104,9 @@ class Agent:
 
         print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
         print('\n')
-        print(self.place_action_list)
+        print("agent 2 place action list: " + str(self.place_action_list))
         print('\n')
-        print(self.place_action_red)
+        print("agent 2 red place action list: " + str(self.place_action_red))
         print('\n')
-        print(self.place_action_blue)
+        print("agent 2 blue place action list: " + str(self.place_action_blue))
         
-
-    # generate a list of possible moves and number of moves given the current state
-    def possible_moves(color: PlayerColor, red, blue):
-        not_expandable_block = red[:] + blue[:]
-
-        blocks = red
-        if color == PlayerColor.BLUE:
-            blocks = blue
-        
-        actions = []
-        for block in blocks:
-            if (block.up(1) not in not_expandable_block):
-                temp_list = []
-                temp_list.append(block.up(1))
-                actions.append(temp_list)
-            if (block.down(1) not in not_expandable_block):
-                temp_list = []
-                temp_list.append(block.down(1))
-                actions.append(temp_list)
-            if (block.left(1) not in not_expandable_block):
-                temp_list = []
-                temp_list.append(block.left(1))
-                actions.append(temp_list)
-            if (block.right(1) not in not_expandable_block):
-                temp_list = []
-                temp_list.append(block.right(1))
-                actions.append(temp_list)
-        # return None if no empty adjacent block
-        if len(actions) == 0: return None
-        # keep adding adjacent red blocks until it is long enough (4 blocks) to form a place action
-        while len(actions[0]) < 4:
-            possible_move = actions.pop(0)
-            for block in list(possible_move):
-                if (block.up(1) not in not_expandable_block and block.up(1) not in list(possible_move)):
-                    clone = list(possible_move)[:]
-                    clone.append(block.up(1))
-                    duplicate = False
-                    for item in actions:
-                        if set(item) == set(clone):
-                            duplicate = True
-                            break
-                    if not duplicate: actions.append(clone)
-                if (block.down(1) not in not_expandable_block and block.down(1) not in list(possible_move)):
-                    clone = list(possible_move)[:]
-                    clone.append(block.down(1))
-                    duplicate = False
-                    for item in actions:
-                        if set(item) == set(clone):
-                            duplicate = True
-                            break
-                    if not duplicate: actions.append(clone)
-                if (block.left(1) not in not_expandable_block and block.left(1) not in list(possible_move)):
-                    clone = list(possible_move)[:]
-                    clone.append(block.left(1))
-                    duplicate = False
-                    for item in actions:
-                        if set(item) == set(clone):
-                            duplicate = True
-                            break
-                    if not duplicate: actions.append(clone)
-                if (block.right(1) not in not_expandable_block and block.right(1) not in list(possible_move)):
-                    clone = list(possible_move)[:]
-                    clone.append(block.right(1))
-                    duplicate = False
-                    for item in actions:
-                        if set(item) == set(clone):
-                            duplicate = True
-                            break
-                    if not duplicate: actions.append(clone)
-        return actions, len(actions)
-
-    # calculate utility of a possible move
-    def utility(color: PlayerColor, action, red, blue):
-        score = 1 + 1
-        return score
-    
-    # return an action based on the utility of given moves by using the minimax strategy
-    # can implement ab pruning in this function
-    def minimax():
-        return None
-    
-    
-
