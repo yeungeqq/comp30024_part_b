91a8fbfe0f429eef16c957641dda4609577d6e8f
JoeyHammoth <jamesputradi@gmail.com>
2024-05-11 20:10:08 +1000

    cleaned random function again

--- part_b/testing_agents/agent2/program.py
+++ part_b/testing_agents/agent2/program.py
@@ -15,6 +15,8 @@ class Agent:
     place_action_list = []
     place_action_red = []
     place_action_blue = []
+    current_red = []
+    current_blue = []
     move_count = 0
 
     def __init__(self, color: PlayerColor, **referee: dict):
@@ -39,7 +41,9 @@ class Agent:
         # Check whether the randomly selected blocks have been placed in the game board
         place_action_coords = []
 
-        action_arr = self.place_action_red if self._color == PlayerColor.RED else self.place_action_blue
+        total_actions_arr = self.current_red + self.current_blue
+
+        action_arr = self.current_red if self._color == PlayerColor.RED else self.current_blue
 
         coord_loop_count = 0
 
@@ -64,12 +68,9 @@ class Agent:
             if down == -1: down = 10
             
             # Check if the coord has been placed on the board
-            for place_action_piece in self.place_action_list:
-                for coord in place_action_piece.coords:
-                    if Coord(xcoord, ycoord) == coord:
-                        coord_already_placed = True
-                        break
-                if coord_already_placed:
+            for coord in total_actions_arr:
+                if Coord(xcoord, ycoord) == coord:
+                    coord_already_placed = True
                     break
             if coord_already_placed:
                 continue
@@ -84,30 +85,21 @@ class Agent:
             elif len(place_action_coords) == 0 and self.move_count != 0:
                 if Coord(xcoord, ycoord) not in place_action_coords:
 
-                    coord_chosen = False
-
-                    for action in action_arr:
-                        for coord in action.coords:
-                            if right == coord.r and ycoord == coord.c:
-                                place_action_coords.append(Coord(xcoord, ycoord))
-                                coord_chosen = True
-                                break
-                            elif left == coord.r and ycoord == coord.c:
-                                place_action_coords.append(Coord(xcoord, ycoord))
-                                coord_chosen = True
-                                break
-                            elif xcoord == coord.r and up == coord.c:
-                                place_action_coords.append(Coord(xcoord, ycoord))
-                                coord_chosen = True
-                                break
-                            elif xcoord == coord.r and down == coord.c:
-                                place_action_coords.append(Coord(xcoord, ycoord))
-                                coord_chosen = True
-                                break
-                            else:
-                                continue
-                        if coord_chosen:
+                    for coord in action_arr:
+                        if right == coord.r and ycoord == coord.c:
+                            place_action_coords.append(Coord(xcoord, ycoord))
                             break
+                        elif left == coord.r and ycoord == coord.c:
+                            place_action_coords.append(Coord(xcoord, ycoord))
+                            break
+                        elif xcoord == coord.r and up == coord.c:
+                            place_action_coords.append(Coord(xcoord, ycoord))
+                            break
+                        elif xcoord == coord.r and down == coord.c:
+                            place_action_coords.append(Coord(xcoord, ycoord))
+                            break
+                        else:
+                            continue
 
             # If this is not the first move of the game and some coords have been chosen
             # then check if the coord is adjacent to any of the coords that has been
@@ -166,8 +158,14 @@ class Agent:
         self.place_action_list.append(action)
         if color == PlayerColor.RED:
             self.place_action_red.append(action)
+            for coord in place_action.coords:
+                self.current_red.append(coord)
         else:
             self.place_action_blue.append(action)
+            for coord in place_action.coords:
+                self.current_blue.append(coord)
+        
+        self.current_red, self.current_blue = self.eliminate_lines(self.current_red, self.current_blue)
 
 
         print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
@@ -177,4 +175,33 @@ class Agent:
         print("agent 2 red place action list: " + str(self.place_action_red))
         print('\n')
         print("agent 2 blue place action list: " + str(self.place_action_blue))
-        
+    
+    def eliminate_lines(self, red, blue):
+        red_clone = red[:]
+        blue_clone = blue[:]
+        block = red_clone + blue_clone
+        eliminated_coords_list = []
+        # check if there are any completed rows. If there are, add these coords to the eliminated_coords_list.
+        for i in range(11):
+            row = []
+            for j in range(11):
+                row.append(Coord(i, j))
+            if set(row).issubset(set(block)):
+                for coord in row:
+                    eliminated_coords_list.append(coord)
+        # check if there are any completed columns. If there are, add these coords to the eliminated_coords_list.
+        for i in range(11):
+            column = []
+            for j in range(11):
+                column.append(Coord(j, i))
+            if set(column).issubset(set(block)):
+                for coord in column:
+                    eliminated_coords_list.append(coord)
+        # Go through the eliminated_coords_list and remove all the non-expandable blocks that are a part of 
+        # any complete rows or columns.
+        for coord in eliminated_coords_list:
+            if coord in red_clone: red_clone.remove(coord)
+            if coord in blue_clone: blue_clone.remove(coord)
+            if coord in block: block.remove(coord)
+        return red_clone, blue_clone
+            
